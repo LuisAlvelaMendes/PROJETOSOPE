2a5e91a2368d3bc3726a96795fcb5cc38c6206d4
Luís Mendes <luis.alvela@gmail.com>
2018-05-06 17:54:19 +0100

    Trying to handle an actual request
    threads bugging out, firing off too early then never again.

--- NEWFINALPROJECT/server.c
+++ NEWFINALPROJECT/server.c
@@ -7,10 +7,12 @@
 #include <sys/stat.h> 
 #include <sys/file.h> 
 #include <pthread.h>
+#include <semaphore.h>
 #include <errno.h> 
 #define MAX_MSG_LEN 20  
 #define MAX_ROOM_SEATS 9999
 #define MAX_CLI_SEATS 99
+#define SHARED 0
 #define WIDTH_PID 5
 #define WIDTH_XXNN 5
 #define WIDTH_SEAT 4
@@ -18,7 +20,10 @@
 
 //utilizar tanto mutex como variavéis de condição
 
-pthread_mutex_t buffer_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  seats_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t  request_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t seats_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t request_lock = PTHREAD_MUTEX_INITIALIZER;
 
 struct Seat
 {
@@ -35,19 +40,23 @@ struct Request
 	char answered; //tells you whether it has been answered or not 'y' means answered and 'n' means unanswered.
 };
 
+sem_t empty, full; //semaphores
+
 struct Request global_current_Request; //the request that will be handled one at a time by the program, the main function receives a request from the FIFO, puts it here if the previous one was answered, and then the threads will take it on and try to reserve a seat.
 
 struct Seat *seats;
+
+int num_room_seats = 0;
 
 int isSeatFree(struct Seat *seats, int seatNum, size_t seatsSize){	
 	
 	for(unsigned int i = 0; i < seatsSize; i++){
 		if(seats[i].seatId == seatNum && seats[i].occupied == 'n'){
-			return 0;
+			return 1;
 		}
 	}
 
-	return 1;
+	return 0;
 }
 
 void bookSeat(struct Seat *seats, int seatNum, int clientId, size_t seatsSize){
@@ -78,10 +87,51 @@ void freeSeat(struct Seat *seats, int seatNum, size_t seatsSize){
 
 void *reserveSeat(void *threadId)
 {
-	pthread_mutex_lock(&buffer_lock);
+	if(global_current_Request.idClient = 0){
+		pthread_exit(NULL);
+	}
 	
+	pthread_mutex_lock(&request_lock);
 	
-	pthread_mutex_unlock(&buffer_lock);
+		pthread_mutex_lock(&seats_lock);
+			
+			write(STDOUT_FILENO, "\n inside lock", 40);			
+	
+			int numValidatedSeats = 0;
+			int validatedIds[global_current_Request.nrIntendedSeats];
+			int clientID = global_current_Request.idClient;
+		
+			for(unsigned int i = 0; i < MAX_CLI_SEATS; i++){
+
+				int seatNum = global_current_Request.idPreferedSeats[i];	
+
+				if(isSeatFree(seats, seatNum, num_room_seats)){
+					write(STDOUT_FILENO, "\n booking seat", 40);
+					bookSeat(seats, seatNum, clientID, num_room_seats);
+					validatedIds[numValidatedSeats] = seatNum;
+					numValidatedSeats++;	
+				}
+			}	
+	
+			// - In case you couldn't validate every seat the costumer wanted, then free all of them.
+			if(numValidatedSeats < global_current_Request.nrIntendedSeats){
+				for(unsigned int a = 0; a < numValidatedSeats; a++){
+					write(STDOUT_FILENO, "\n can't book", 40);
+					freeSeat(seats, validatedIds[a], num_room_seats);
+				}
+			}
+			
+			for(unsigned int a = 0; a < numValidatedSeats; a++){
+				write(STDOUT_FILENO, "\n validated", 40);
+			}
+
+		pthread_mutex_unlock(&seats_lock);
+	
+	global_current_Request.answered = 'y';
+	
+	pthread_cond_signal(&request_cond);
+	pthread_mutex_unlock(&request_lock);
+	
 	pthread_exit(NULL);
 }
 
@@ -120,6 +170,9 @@ int validate_request_parameters(struct Request r1, int num_room_seats){
 		printf("Invalid answer character '%c'\n", r1.answered);
 		return -4;
 	}
+	
+	//valid request
+	return 0;
 }
 
 int main(int argc, char* argv[]){
@@ -135,7 +188,7 @@ int main(int argc, char* argv[]){
 	}
 
 	//2. Getting args
-	int num_room_seats = atoi(argv[1]);
+	num_room_seats = atoi(argv[1]);
     	int num_ticket_offices = atoi(argv[2]);
 	int open_time = atoi(argv[3]);
 
@@ -145,8 +198,18 @@ int main(int argc, char* argv[]){
 	memset(global_current_Request.idPreferedSeats, 0, MAX_CLI_SEATS);
 	global_current_Request.answered = 'n';
 
-	// - Initializing Seats
+	// - Initializing Seats array and Seat objects
 	seats = (struct Seat*)malloc(sizeof(struct Seat)*num_room_seats);
+	
+	for(unsigned int i = 0; i < num_room_seats; i++){
+		struct Seat currentSeat;
+
+		currentSeat.seatId = (i+1);
+		currentSeat.clientId = 0;
+		currentSeat.occupied = 'n';
+		
+		seats[i] = currentSeat;
+	}
 
 	//3. Making Fifo
 	if (mkfifo("/tmp/requests",0660)<0) 
@@ -160,7 +223,7 @@ int main(int argc, char* argv[]){
 	int thrArg[num_ticket_offices]; 
 
 	for (t=1; t<= num_ticket_offices; t++){ 
-		printf("Creating thread %d\n", t); 
+		write(STDOUT_FILENO, "\n Creating thread", 20); 
 		thrArg[t-1] = t; 
 		rc = pthread_create(&tid[t-1], NULL, reserveSeat, &thrArg[t-1]); 
 
@@ -173,13 +236,36 @@ int main(int argc, char* argv[]){
 	if ((fd=open("/tmp/requests",O_RDONLY)) !=-1) printf("FIFO '/tmp/requests' openned in READONLY mode\n"); 
 
 	do { 
-		//reading from FIFO    		
-		n=read(fd,&global_current_Request,sizeof(struct Request)); 
+		// - Reading from FIFO, first into a "dummy variable" to check if the input is even valid or not
+		struct Request tempRequest;		
+
+		n=read(fd,&tempRequest,sizeof(struct Request)); 
 		
-		if (n>0) printf("%d client request has arrived\n", global_current_Request.idClient);
+		if (n>0) printf("%d client request has arrived\n", tempRequest.idClient);
 		
-		//checking if what was read has valid parameters
-		validate_request_parameters(global_current_Request, num_room_seats);
+		// - Validating
+		if(validate_request_parameters(tempRequest, num_room_seats) != 0){
+			break;
+		}
+
+		// - Placing the request in the buffer, locking it between critical zone.		 
+		pthread_mutex_lock(&request_lock);
+  		
+		global_current_Request.idClient = tempRequest.idClient;
+		global_current_Request.nrIntendedSeats = tempRequest.nrIntendedSeats;
+
+		for(unsigned int i = 0; i < MAX_CLI_SEATS; i++){
+			global_current_Request.idPreferedSeats[i] = tempRequest.idPreferedSeats[i];
+		}
+
+		global_current_Request.answered = tempRequest.answered;
+	
+		// - Wait until the request has been answered.
+		while(global_current_Request.answered != 'y'){		
+			pthread_cond_wait(&request_cond, &request_lock);
+		}
+		
+		pthread_mutex_unlock(&request_lock);
 
 	} while (global_current_Request.idClient != -1); 
   	
